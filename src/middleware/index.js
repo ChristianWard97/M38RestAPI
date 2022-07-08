4615eb23634c0fd501cc49b55be2d2e3620df892
Christian Ward <christianward97@outlook.com>
2022-07-08 15:35:25 +0100

    latest update

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const User = require("../user/model");
 
 
 exports.hashPass = async (req, res, next) => {
@@ -10,3 +11,34 @@ try {
     res.send({ error });
 }
 };
+
+exports.checkPass = async (req, res, next) => {
+    try{
+        const userCheck = await User.findOne({
+            username: req.body.username,})
+            const check = await bcrypt.compare(req.body.password, userCheck.password);
+            if(check){
+                next();
+            }
+            else{
+                throw new Error("Incorrect Credentials");
+            }
+    } catch(error){
+        console.log(error);
+        res.send({error});
+    }
+};
+
+exports.tokenCheck = async (req, res, next) => {
+    try {
+      const decodedToken = jwt.verify(
+        req.header("Authorization"),
+        process.env.SECRET
+      );
+      req.user = await User.findById(decodedToken.id);
+      next();
+    } catch (error) {
+      console.log(error);
+      res.send({ error });
+    }
+  };
